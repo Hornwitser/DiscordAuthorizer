Hornwitser/DiscordAuthorizer

commit a064d9df6e9774813047507418a5d41b3ab77e2e

Delay sending user updates
When another DataWriter updates the User class, a nested transaction is
done, which means that the _postSaveAfterTransaction hook is executed
still inside the ongoing transaction.  This means that the update
notification sent to the bot will happen before the data is actually
committed to the database.

Fix by delaying the update notification until after the current
controller has finished dispatching it's action.  This is done by
registring a CodeEvent listener in the _postSave and _postDelete hooks
for the User DataWriter.

xenforo/library/DiscordAuth/DataWriter/User.php
@@ -63,27 +63,33 @@ class DiscordAuth_DataWriter_User
         // Todo: Catch eceptions and log them.
     }
 
-    public function _postSaveAfterTransaction()
+    public function _postSave()
     {
-        parent::_postSaveAfterTransaction();
+        parent::_postSave();
 
         $discordId = $this->getExisting('da_discord_id');
         if ($discordId !== null) {
-            self::refreshDiscordId($discordId);
+            XenForo_CodeEvent::addListener(
+                'controller_post_dispatch',
+                function ($c, $r, $n, $a) use ($discordId) {
+                    self::refreshDiscordId($discordId);
+                }
+            );
         }
     }
 
-    // Unfortunately there's no _postDeleteAfterTransaction
-    public function delete()
+    public function _postDelete()
     {
+        parent::_postDelete();
+
         $discordId = $this->getExisting('da_discord_id');
-
-        $result = parent::delete();
-
         if ($result && $discordId !== null) {
-            self::refreshDiscordId($discordId);
+            XenForo_CodeEvent::addListener(
+                'controller_post_dispatch',
+                function ($c, $r, $n, $a) use ($discordId) {
+                    self::refreshDiscordId($discordId);
+                }
+            );
         }
-
-        return $result;
     }
 }

commit aa8427f53ec68df3fbed409eb38b45e463dcdfcb

Fix User delete broken in a064d9d
The rewrite in a064d9d did not remove the $result variable from the if
condition when delete() was rewritten to _postDelete().  This caused a
fatal error to be shown when a user was merged or deleted.  Fix by
removing the reference to the $result variable.

xenforo/library/DiscordAuth/DataWriter/User.php
@@ -84,7 +84,7 @@ class DiscordAuth_DataWriter_User
         parent::_postDelete();
 
         $discordId = $this->getExisting('da_discord_id');
-        if ($result && $discordId !== null) {
+        if ($discordId !== null) {
             XenForo_CodeEvent::addListener(
                 'controller_post_dispatch',
                 function ($c, $r, $n, $a) use ($discordId) {